42db4b001141136ee28ecb77cfb27bcb3b6bb315
Samip Nepal <154350896+Samip-Nepal@users.noreply.github.com>
2024-10-05 20:47:59 +1000

    Getting start with the react

---
+++ src/header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+        <>
+        <h1>My website</h1><nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Play</a></li>
+                <li><a href="#"> Back to Home</a></li>
+            </ul>
+        </nav>
+        <hr />
+        </>
+    );
+};
+
+export default Header;
