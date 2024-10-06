37d3a9ca7557695678615ee02d55831f6452dde8
Samip Nepal <154350896+Samip-Nepal@users.noreply.github.com>
2024-10-06 22:47:58 +1100

    Simple react with js and component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import Header from "./header.jsx";
-
+import Footer from "./footer.jsx";
+import Food from "./Food.jsx";
 function App() {
   return (
+    <>
     <Header />
+    <Footer />
+    <Food/>
+    </>
+
   );
 }
 
---
+++ src/Food.jsx
@@ -0,0 +1,13 @@
+function Food() {
+    const food1="orange";
+    const food2="Apple"
+    return(
+<ul>
+    <li>{food1}</li>
+    <li>{food2}</li>
+
+</ul>
+    );
+    
+}
+export default Food;
